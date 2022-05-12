18623332ea5a007a188a31c7e56fa9dc1ec9d014
Neha <ran.danu789@gmail.com>
2022-05-12 12:47:55 +0530

    added js files

---
+++ News/News.js
@@ -0,0 +1,58 @@
+import { StyleSheet, Text, View, ImageBackground, Image, ScrollView } from 'react-native';
+import { transform } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import { events } from '../data/Event';
+import Footer from './Footer';
+import { Events } from '../data/Event';
+import EventMain from './EventMain';
+import EventDetails from './EventDetails';
+import NewsMain from './NewsMain';
+import { Newsdata } from '../data/Newsdata';
+import { useEffect } from 'react';
+import { get } from '../services/http-service';
+
+
+
+export default function News() {
+
+   let newsData = [];
+
+  useEffect(() => {
+    const results = get('/api/news').then(res => {
+      newsData = res;
+      console.log(newsData)
+    }).catch(err => {
+      console.log(err)
+    });
+  }, []);
+
+  return (
+      <View style={styles.container}>
+        <ScrollView style={styles.img} showsVerticalScrollIndicator={false}>
+          {newsData.map((news,index)=>{
+            console.log("==news== ", news)
+            return(
+            <NewsMain post={news} key={index}/>
+          )})}
+        </ScrollView>
+        
+        
+      </View>
+      
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    display:'flex',
+    flexDirection:'column',
+
+    
+  },
+  img:{
+    height:300,
+    zIndex:100
+  }
+
+});
---
+++ News/NewsMain.js
@@ -0,0 +1,89 @@
+import { View, Text, StyleSheet,TouchableOpacity, Image, ScrollView} from 'react-native'
+import React from 'react';
+import EventHeader from './EventHeader';
+import { Divider } from 'react-native-elements';
+import { Events } from '../data/Event';
+import Eventfooter from './Eventfooter';
+import EventDetails from './EventDetails';
+import NewsDetails from './NewsDetails';
+import { Newsdata } from '../data/Newsdata';
+import { useNavigation } from "@react-navigation/native";
+
+
+const NewsMain = ({post}) => {
+    const navigation = useNavigation()
+
+    useEffect(() => {
+      console.log(post)
+    
+    }, [])
+    
+
+  return (
+    <View style={styles.main} >
+      <EventHeader post={post}/>
+      {/*<View style={styles.detail2}>
+        <Text style={styles.text2}>{post.Date} </Text>
+        <Text style={styles.text3}>{ post.Month.length > 4? post.Month.slice(0, 3) : post.Month}</Text>
+      </View>*/}
+      <TouchableOpacity
+        onPress={()=>navigation.navigate("NewsDetails")}
+        style={styles.container}>
+
+            <Image style={styles.img} source={{uri:post.imagepath}}  />
+
+      </TouchableOpacity>
+      <NewsDetails post={post}/>
+      <Divider width={1} orientation='vertical'/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+      main:{
+        flex:1,
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+
+      },
+    container:{
+        marginTop:10,
+        marginBottom:15,
+        
+    },
+    detail1:{
+      alignSelf:'center',
+      alignItems:'center',
+      textAlign:'center',
+      width:300,
+    },
+     detail2:{
+      position:'absolute',
+      marginTop:180,
+      marginLeft:300,
+      paddingHorizontal:30,
+      paddingVertical:25,
+      borderRadius:15,
+      backgroundColor:'rgba(255, 255, 255,0.7)',
+      zIndex:100,
+      
+    },
+    img:{
+        resizeMode:'cover',
+        width:'100%',
+        height:450
+    },
+    text3:{
+      color:'black',
+      textAlign:'center',
+      fontSize:20
+    },
+    text2:{
+      color:'#696969',
+      textAlign:'center',
+      fontSize:27
+    }
+})
+
+export default NewsMain;
